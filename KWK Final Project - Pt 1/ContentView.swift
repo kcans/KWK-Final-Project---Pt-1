63dbe33a07984febae8c7da23057173c8a7fce44
Karen Canseco <kar3n.c35@gmail.com>
2023-06-26 10:56:41 -0700

    Initial Commit

---
+++ KWK Final Project - Pt 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KWK Final Project - Pt 1
+//
+//  Created by Karen Canseco on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
